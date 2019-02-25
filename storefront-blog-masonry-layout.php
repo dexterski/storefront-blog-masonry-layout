ea588d0d53de2767b7eb9b1dac4f80eaddcd3ac4
dexterski <47657799+dexterski@users.noreply.github.com>
2019-02-25 09:55:01 +0100

    Fixed footer pagination
    Pagination now is showing correctly, and styles loads only on blog posts page.

--- storefront-blog-masonry-layout.php
+++ storefront-blog-masonry-layout.php
@@ -3,7 +3,7 @@
 Plugin Name: StoreFront Blog masonry grid layout
 Plugin URI: https://bogaczek.com
 Description: This plugin creates fully automatic, responsive masonry grid for Storefront blog, using pure CSS. For WooCommerce StoreFront theme only (but should work with other themes too – may need some CSS tweaking).
-Version: 0.7
+Version: 0.8
 Author: Black Sun
 Author URI: https://bogaczek.com
 Text Domain: storefront-blog-masonry-layout
@@ -12,8 +12,11 @@ defined('ABSPATH') or die();
 
 // Enqueuing basic styles for plugin
 function dexter_storefront_blog_masonry_layout() {
-	wp_enqueue_style( 'dexter-storefront-blog-masonry-layout-style', plugins_url( 'assets/css/style.css', __FILE__ ) );
+	if (is_home()) {
+		wp_enqueue_style( 'dexter-storefront-blog-masonry-layout-style', plugins_url( 'assets/css/style.css', __FILE__ ) );
+		remove_action( 'storefront_loop_after', 'storefront_paging_nav', 10);
+		add_action ( 'storefront_before_footer', 'storefront_paging_nav', 10);
+	}
 }
-
 add_action('wp_enqueue_scripts', 'dexter_storefront_blog_masonry_layout', 666 );
 ?>
